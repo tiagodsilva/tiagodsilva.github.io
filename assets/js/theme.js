aa3866bbd8d09cd3c28dc1344610ca2ddf1523f6
Tiago da Silva <tdsh97@gmail.com>
2024-10-13 16:27:53 -0300

    change default theme to light

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -232,18 +232,18 @@ let determineComputedTheme = () => {
 };
 
 let initTheme = () => {
-  let themeSetting = determineThemeSetting();
+  let themeSetting = "light"; // determineThemeSetting();
 
   setThemeSetting(themeSetting);
 
   // Add event listener to the theme toggle button.
-  document.addEventListener("DOMContentLoaded", function () {
-    const mode_toggle = document.getElementById("light-toggle");
+  // document.addEventListener("DOMContentLoaded", function () {
+  //   const mode_toggle = document.getElementById("light-toggle");
 
-    mode_toggle.addEventListener("click", function () {
-      toggleThemeSetting();
-    });
-  });
+  //   mode_toggle.addEventListener("click", function () {
+  //     toggleThemeSetting();
+  //   });
+  // });
 
   // Add event listener to the system theme preference change.
   window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", ({ matches }) => {
